309eea4fa62019bb65ffbdcd06273aae91895171
Student <Student@Train10-4-PC090.control.ad.sit.kmutt.ac.th>
2025-09-24 11:41:33 +0700

    add a second way

--- week7/app.js
+++ week7/app.js
@@ -1,14 +1,19 @@
 // create p
 const pElement = document.createElement('p');
 // create id = "p5"
-const idAttr = document.createAttribute('id');
-idAttr.value = 'p5';
+// const idAttr = document.createAttribute('id');
+// idAttr.value = 'p5';
+
+pElement.setAttribute('id', 'p5');
 // binding id=P5 to p element
-pElement.setAttributeNode(idAttr);
+// pElement.setAttributeNode(idAttr);
 // create text node
-const textNode = document.createTextNode('Jookroo lakron');
+// solution 1 : const textNode = document.createTextNode('Jookroo lakron');
+
+// solution 2 : 
+pElement.textContent = 'Jookroo lakron';
 // bindjg text node to p element
-pElement.appendChild(textNode);
+// solution 2 not need binding  pElement.appendChild(textNode);
 // binding p id to body
 const body = document.body;
 body.appendChild(pElement);
